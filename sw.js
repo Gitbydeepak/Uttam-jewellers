e7c1c53e7aaa8d80d76116685649c4e5b11035dd
Deepak <deepakjaiswar0607@gmail.com>
2026-04-05 11:24:28 +0530

    Update service worker cache and event listeners

--- sw.js
+++ sw.js
@@ -1,36 +1,20 @@
-const CACHE_NAME = 'Uttam Jewellers-v2';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
+const CACHE_NAME = 'uttam-v1';
+const urlsToCache = ['/', '/index.html', '/manifest.json', '/icon.png'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
+  event.waitUntil(caches.keys().then(cacheNames => {
+    return Promise.all(cacheNames.map(cache => {
+      if (cache !== CACHE_NAME) return caches.delete(cache);
+    }));
+  }));
   self.clients.claim();
 });
